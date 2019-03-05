d41cfd8e87b679307171d67fc067d4a3b2fa035d
Patrick McDonnell <patrick.g.mcdonnell@gmail.com>
2019-03-05 20:45:53 +0000

    add basic channel for game

---
+++ lib/islands_interface_web/channels/game_channel.ex
@@ -0,0 +1,10 @@
+defmodule IslandsInterfaceWeb.GameChannel do
+  use IslandsInterfaceWeb, :channel
+
+  alias IslandsInterface.{Game, GameSupervisor}
+
+  def join("game:" <> _player, _payload, socket) do
+    {:ok, socket}
+  end
+
+end
