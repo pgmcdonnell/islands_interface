5819defc807826d19c2d2e97f9949a5ea82f0c75
Patrick McDonnell <patrick.g.mcdonnell@gmail.com>
2019-03-05 12:52:25 +0000

    add IslandsEngine as a dependency

--- mix.exs
+++ mix.exs
@@ -39,7 +39,8 @@ defmodule IslandsInterface.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:cowboy, "~> 2.0"},
-      {:plug_cowboy, "~>2.0"}
+      {:plug_cowboy, "~>2.0"},
+      {:islands_engine, path: "../islands_engine"}
     ]
   end
 end
