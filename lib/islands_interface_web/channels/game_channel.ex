a9085e8261c1703699f61a32f40797a9f4f9ed27
Patrick McDonnell <patrick.g.mcdonnell@gmail.com>
2019-03-06 20:56:56 +0000

    add working phoenix channel

--- lib/islands_interface_web/channels/game_channel.ex
+++ lib/islands_interface_web/channels/game_channel.ex
@@ -3,8 +3,13 @@ defmodule IslandsInterfaceWeb.GameChannel do
 
   alias IslandsInterface.{Game, GameSupervisor}
 
-  def join("game:" <> _player, _payload, socket) do
+  def join("game:" <> player, _payload, socket) do
+    IO.puts "Joining the game channel"
     {:ok, socket}
+  end
+
+  def handle_in("hello", payload, socket) do
+    {:reply, {:ok, payload}, socket}
   end
 
 end
