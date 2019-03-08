8a57a2a2e31a068e657b0baaed50c7759c40e550
Patrick McDonnell <patrick.g.mcdonnell@gmail.com>
2019-03-08 20:57:59 +0000

    add positioning and setting of islands to interface

---
+++ assets/js/game_commands.js
@@ -0,0 +1,79 @@
+//join channel
+var socket = new Phoenix.Socket("/socket", {params: {token: window.userToken}})
+socket.connect()
+function new_channel(subtopic, screen_name) {
+    return socket.channel("game:" + subtopic, {screen_name: screen_name});
+}
+var game_channel = new_channel("moon", "moon")
+function join(channel) {
+    channel.join()
+    .receive("ok", response => {
+        console.log("joined sucessfully!", response)
+    })
+    .receive("error", response => {
+        console.log("unable to join", response)
+    })
+}
+join(game_channel)
+
+//start new game (following joining commands)
+function new_game(channel) {
+    channel.push("new_game")
+    .receive("ok", response => {
+        console.log("New Game!", response)
+    })
+    .receive("error", response => {
+        console.log("Unable to start a new game.", response)
+    })
+}
+new_game(game_channel)
+
+//make visible when a new player joins
+game_channel.on("player_added", response => {
+    console.log("Player Added", response)
+})
+
+//adding a second player to a game (after join command, in second browser)
+function add_player(channel, player) {
+    channel.push("add_player", player)
+    .receive("error", response => {
+        console.log("unable to add new player: " + player, response)
+    })
+}
+add_player(game_channel, "PlayerName")
+
+//positioning islands
+function position_island(channel, player, island, row, col) {
+  var params = {"player": player, "island": island, "row": row, "col": col}
+  channel.push("position_island", params)
+  .receive("ok", response => {
+    console.log("Island positioned!", response)
+  })
+  .receive("error", response => {
+    console.log("Unable to position island.", response)
+  })
+}
+
+//handy positions for all islands
+position_island(game_channel, "player2", "atoll", 1, 1)
+position_island(game_channel, "player2", "dot", 1, 5)
+position_island(game_channel, "player2", "l_shape", 1, 7)
+position_island(game_channel, "player2", "s_shape", 5, 1)
+position_island(game_channel, "player2", "square", 5, 5)
+
+//setting islands once positioned
+function set_islands(channel, player) {
+  channel.push("set_islands", player)
+  .receive("ok", response => {
+    console.log("Here is the board:")
+    console.dir(response.board)
+  })
+  .receive("error", response => {
+    console.log("Unable to set islands for: " + player, response)
+  })
+}
+
+//listen for a response
+game_channel.on("player_set_islands", response => {
+  console.log("Player set islands", response)
+})
--- lib/islands_interface_web/channels/game_channel.ex
+++ lib/islands_interface_web/channels/game_channel.ex
@@ -1,15 +1,55 @@
 defmodule IslandsInterfaceWeb.GameChannel do
   use IslandsInterfaceWeb, :channel
 
-  alias IslandsInterface.{Game, GameSupervisor}
+  alias IslandsEngine.{Game, GameSupervisor}
 
   def join("game:" <> player, _payload, socket) do
     IO.puts "Joining the game channel"
     {:ok, socket}
   end
 
-  def handle_in("hello", payload, socket) do
-    {:reply, {:ok, payload}, socket}
+  def handle_in("new_game", _payload, socket) do
+    "game:" <> player = socket.topic
+    case GameSupervisor.start_game(player) do
+      {:ok, _pid} ->
+        IslandsEngine.Island.types() #ensures our atoms exist for later
+        {:reply, :ok, socket}
+      {:error, reason} ->
+        {:reply, {:error, %{reason: inspect(reason)}}, socket}
+    end
   end
+
+  def handle_in("add_player", player, socket) do
+    case Game.add_player(via(socket.topic), player) do
+      :ok ->
+        broadcast! socket, "player_added", %{message: "new player just joined:" <> player}
+        {:noreply, socket}
+      {:error, reason} ->
+        {:reply, {:error, %{reason: inspect(reason)}}, socket}
+      :error -> {:reply, :error, socket}
+    end
+  end
+
+  def handle_in("position_island", payload, socket) do
+    %{"player" => player, "island" => island, "row" => row, "col" => col} = payload
+    player = String.to_existing_atom(player)
+    island = String.to_existing_atom(island)
+    case Game.position_island(via(socket.topic), player, island, row, col) do
+      :ok -> {:reply, :ok, socket}
+      _ -> {:reply, :error, socket}
+    end
+  end
+
+  def handle_in("set_islands", player, socket) do
+    player = String.to_existing_atom(player)
+    case Game.set_islands(via(socket.topic), player) do
+      {:ok, board} ->
+        broadcast! socket, "player_set_islands", %{player: player}
+        {:reply, {:ok, %{board: inspect(board)}}, socket}
+      _ -> {:reply, :error, socket}
+    end
+  end
+
+  defp via("game:" <> player), do: Game.via_tuple(player)
 
 end
